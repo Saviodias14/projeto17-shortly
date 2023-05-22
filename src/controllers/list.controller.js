3009c75a2a4e5de21e4efee4a53fa74beded4184
Savio Dias Viana <saviodiasviana@gmail.com>
2023-05-22 01:24:35 -0300

    Correção de problema no getInfoUser

--- src/controllers/list.controller.js
+++ src/controllers/list.controller.js
@@ -5,16 +5,16 @@ export async function getInfoUser(req, res) {
     try {
         const result = await db.query(`
         SELECT url.id, url."userId", u.name, url.url, url."shortUrl", url."visitCount", total."totalVisitCount"
-        FROM usuarios u
-        JOIN urls url ON u.id = url."userId"
-        JOIN (
-          SELECT "userId", SUM("visitCount") AS "totalVisitCount"
-          FROM urls
-          GROUP BY "userId"
-        ) total ON u.id = total."userId"
-        WHERE url."userId" = $1;`, [userId])
+FROM usuarios u
+LEFT JOIN urls url ON u.id = url."userId" AND url."userId" = $1
+JOIN (
+  SELECT "userId", SUM("visitCount") AS "totalVisitCount"
+  FROM urls
+  GROUP BY "userId"
+) total ON u.id = total."userId";`, [userId])
         const { name, totalVisitCount } = result.rows[0]
-        const userInfo = { id: userId, name, visitCount: totalVisitCount, shortenedUrls: [] }
+        const userInfo = { id: userId, name: name, visitCount: totalVisitCount, shortenedUrls: [] }
+        console.log(result.rows)
         result.rows.map((e) => userInfo.shortenedUrls.push({ id: e.id, shortUrl: e.shortUrl, url: e.url, visitCount: e.visitCount }))
         res.status(200).send(userInfo)
     } catch (err) {
@@ -32,7 +32,7 @@ export async function getRanking(req, res) {
           FROM urls
           GROUP BY "userId"
         ) total ON u.id = total."userId"
-        ORDER BY total."visitCount"
+        ORDER BY total."visitCount" DESC
         LIMIT 10;`)
         res.status(200).send(result.rows)
 
